20cee28e6970789d375b79b0738c4cfc8e0823d6
Phil Tallini <philiptallini@gmail.com>
2018-12-23 22:26:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  if (animal === 'dog') {
-    return 'cat';
-  }
-  if (animal === 'cat') {
-    return 'dog';
-  }
+  return animal;
 }
 
 function add2(n) {
